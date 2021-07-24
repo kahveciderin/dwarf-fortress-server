4ffd940f2680041314854acbb499b8acc464f913
kahveciderin <kahveciderin@gmail.com>
2021-07-24 22:29:23 +0300

    add instructions

--- install-df.sh
+++ install-df.sh
@@ -2,4 +2,4 @@ mkdir ~/dwarf_fortress 2> /dev/null || echo "Dwarf fortress directory already ex
 mkdir ~/dwarf_fortress/saves 2> /dev/null || echo "Save directory already exists, skipping..."
 wget https://raw.githubusercontent.com/kahveciderin/dwarf-fortress-server/master/Dockerfile
 docker build -t dfserver . && docker run --restart always --privileged -p 8765:1234 -v ~/dwarf_fortress/saves:/df/df_linux/data/save -it dfserver
-echo "Server should be up and running!"
+echo "Server should be up and running on port 8765!"
--- install-docker.sh
+++ install-docker.sh
@@ -1,9 +1,7 @@
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
-sudo apt install docker-ce
+sudo apt install docker-ce -y
 sudo usermod -aG docker ${USER}
-echo "Installation successful, please log out, log back in and run:"
-echo ""
