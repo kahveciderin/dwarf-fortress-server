94204ec32b8e8e5fab88f5a09f6da59f3994a364
kahveciderin <kahveciderin@gmail.com>
2021-07-24 23:06:03 +0300

    split run command to two lines

--- install-df.sh
+++ install-df.sh
@@ -3,5 +3,6 @@ mkdir ~/dwarf_fortress/saves 2> /dev/null || echo "Save directory already exists
 mkdir df_install
 cd df_install
 wget https://raw.githubusercontent.com/kahveciderin/dwarf-fortress-server/master/Dockerfile
-docker build -t dfserver . && docker run --restart always --privileged -p 8765:1234 -v ~/dwarf_fortress/saves:/df/df_linux/data/save -it dfserver
+docker build -t dfserver .
+docker run -d --restart always --privileged -p 8765:1234 -v ~/dwarf_fortress/saves:/df/df_linux/data/save -it dfserver
 echo "Server should be up and running on port 8765!"
