3fc547b2291f491aac651f82473eeac07531a9c4
kahveciderin <kahveciderin@gmail.com>
2021-07-25 09:50:15 +0300

    add fail case to install script

--- install-df.sh
+++ install-df.sh
@@ -1,3 +1,6 @@
+failed=
+trap 'failed=yes' ERR
+
 mkdir ~/dwarf_fortress 2> /dev/null || echo "Dwarf fortress directory already exists, skipping..."
 mkdir ~/dwarf_fortress/saves 2> /dev/null || echo "Save directory already exists, skipping..."
 mkdir df_install
@@ -5,4 +8,11 @@ cd df_install
 wget https://raw.githubusercontent.com/kahveciderin/dwarf-fortress-server/master/Dockerfile
 docker build -t dfserver .
 docker run -d --restart always --privileged -p 8765:1234 -p 5000:5000 -v ~/dwarf_fortress/saves:/df/df_linux/data/save -it dfserver
-echo "Server should be up and running on port 8765!"
+cd ..
+rm -rf df_install/
+if [ -z "$failed" ]; then
+    echo "Server should be up and running on port 8765!"
+else
+    echo "Encountered error, deleting created docker container"
+    docker stop dfserver
+    docker rm dfserver
